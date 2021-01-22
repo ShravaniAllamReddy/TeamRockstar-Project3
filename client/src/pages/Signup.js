54742ecd23cfaf766a7a99f98687feb16c801ca1
Luis <lgd26308@gmail.com>
2021-01-21 19:47:10 -0700

    added sign up page

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,8 +1,43 @@
 import { useState } from 'react';
 import { Redirect, useHistory, useLocation } from 'react-router-dom';
 import useAuth from '../hooks/auth';
+import React from 'react';
+// import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+// import FormControlLabel from '@material-ui/core/FormControlLabel';
+// import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+// import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(1),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+}));
 
 const Signup = () => {
+    const classes = useStyles();
     const { signup, isLoggedIn } = useAuth();
     // History and location are hooks we can use to manipulate our page's history!
     const history = useHistory();
@@ -36,38 +71,68 @@ const Signup = () => {
     }
 
     return (
-        <div>
-            <h2>
-                Signup Page
-            </h2>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor='email'>Email:</label>
-                <input
-                    name='email'
-                    placeholder='Email'
-                    type='email'
-                    autoComplete='username'
-                    value={email}
-                    onChange={event => setEmail(event.target.value)}
-                />
-                <br />
-                <label htmlFor='password'>Password:</label>
-                <input
-                    name='password'
-                    placeholder='Password'
-                    type='password'
-                    autoComplete='password'
-                    value={password}
-                    onChange={event => setPassword(event.target.value)}
-                />
-                <br />
-                <button type='submit'>Signup</button>
-            </form>
-            <p>
-                Already have an account? <button onClick={() => toggleRedirect(true)}>Login Here</button>
-            </p>
+        <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <div className={classes.paper}>
+                {/* <Avatar className={classes.avatar}>
+                <LockOutlinedIcon />
+            </Avatar> */}
+                <Typography component="h1" variant="h5">
+                    Sign up
+                </Typography>
+                <form onSubmit={handleSubmit}>
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="email"
+                        label="Email Address"
+                        name="email"
+                        autoComplete="email"
+                        autoFocus
+                        value={email}
+                        onChange={event => setEmail(event.target.value)}
+                    />
+                    <br />
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="password"
+                        label="Password"
+                        type="password"
+                        id="password"
+                        autoComplete="current-password"
+                        value={password}
+                        onChange={event => setPassword(event.target.value)}
+                    />
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        className={classes.submit}
+                    >
+                        Sign Up
+                    </Button>
+                    <Grid container>
+                        <Grid item xs>
+                        </Grid>
+                        <Grid item>
+                            <p>
+                                Have an account? <Link href="#" variant="body2" onClick={() => toggleRedirect(true)}>Sign in Here</Link>
+                            </p>
+                        </Grid>
+                    </Grid>
+                </form>
 
-        </div>
+
+            </div>
+            <Box mt={8}>
+            </Box>
+        </Container>
     );
 };
 
