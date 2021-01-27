954850cd8217cce4aaa8f57d4c715ce6d4609bbb
Luis <lgd26308@gmail.com>
2021-01-26 21:20:39 -0700

    added drawer function and home page layout

--- client/src/components/Home/About.js
+++ client/src/components/Home/About.js
@@ -1,12 +1,35 @@
 import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+
+    heroContent: {
+        backgroundColor: theme.palette.background.paper,
+        padding: theme.spacing(8, 0, 6),
+    },
+}));
 
 function About() {
+    const classes = useStyles();
+
     return (
         <Container>
-            <div>
-                <h1>Event Planner</h1>
-                <p>About the app</p>
+            <CssBaseline />
+            <div className={classes.heroContent}>
+                <Container maxWidth="sm">
+                    <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+                Event Planner
+                    </Typography>
+                    <Typography variant="h5" align="center" color="textSecondary" paragraph>
+                Something short and leading about the collection below—its contents, the creator, etc.
+                Make it short and sweet, but not too short so folks don&apos;t simply skip over it
+                entirely.
+                    </Typography>
+                </Container>
             </div>
+            
         </Container>
     );
 }
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,19 +1,31 @@
 import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import SimpleMenu from './Menu';
+// import SimpleMenu from './Menu';
+import Drawer from './Drawer';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
 // import { Link } from 'react-router-dom';
 // import useAuth from '../hooks/auth';
+
+const useStyles = makeStyles((theme) => ({
+    title: {
+        flexGrow: 1,
+    },
+}));
 
 
 const NavBar = () => {
     // const { isLoggedIn, logout, getProfile } = useAuth();
-
+    const classes = useStyles();
 
     return (
         <AppBar position="sticky">
             <Toolbar>
-                <SimpleMenu />
+                <Drawer />
+                <Typography variant="h6" className={classes.title}>
+                    Event Planner
+                </Typography>
             </Toolbar>
         </AppBar>
     );
