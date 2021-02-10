3d9a9d50269fabcf659ca11c279aa89162dd197d
shravaniallamreddy <shravani.allam@gmail.com>
2021-02-10 12:43:43 -0700

    modified code to go to particular activity

--- client/src/components/ActivityResult.js
+++ client/src/components/ActivityResult.js
@@ -5,7 +5,10 @@ import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import axios from 'axios';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import API from '../utils/API';
+import Container from '@material-ui/core/Container';
 
 const useStyles = makeStyles((theme) => ({
     paper: {
@@ -22,10 +25,20 @@ const useStyles = makeStyles((theme) => ({
     }
 }));
 // this is where we display food and movie options in cards so that user can vote from those choices ( invited users) food options from zomato api checkbox
-const ActivityResult = (props) => {
+function ActivityResult(props) {
     const classes = useStyles();
+    const [activity, setActivity] = useState({});
     const [foodVoted, setFoodVoted] = useState('');
     const [movieVoted, setMovieVoted] = useState('');
+
+    // When this component mounts, grab the activity with the _id of props.match.params.id
+    // e.g. localhost:3000/activities/activityId
+    const { id } = useParams();
+    useEffect(() => {
+        API.getActivityById(id)
+            .then(res => setActivity(res.data))
+            .catch(err => console.log(err));
+    }, []);
 
     // to post the votes
     const handleSubmit = (event) => {
@@ -43,72 +56,54 @@ const ActivityResult = (props) => {
             }
         );
     };
-    console.log(props.savedActivities);
-    return props.savedActivities.length > 0 ? (
-        <div className="card">
-            <div className="card-body">
-                {props.savedActivities.map((activity) => {
-                    return (
-                        <div className='text-center' key={activity._id}>
 
-                            <h3 className="font-weight-bold">{activity.name}</h3>
+    return (
+        <Container>
+            <div className="card">
+                <div className="card-body">
 
-                            <p> {activity.description}</p>
-                            <Typography component="h5" variant="h6">
-                                Near By Restaurants
-                            </Typography>
-                            <FoodOptions
-                                activity={activity}
-                                setFoodVoted={setFoodVoted}
-                                foodVoted={foodVoted}
-                            />
-                            <hr />
-                            <Typography component="h5" variant="h6">
-                                Popular Movies on 'The Movie DB'
-                            </Typography>
-                            <MovieOptions
-                                activity={activity}
-                                setMovieVoted={setMovieVoted}
-                                movieVoted={movieVoted}
-                            />
-                            <Button
-                                type="submit"
-                                variant="contained"
-                                color="primary"
-                                className={classes.submit}
-                                onClick={handleSubmit}
-                            >
-                                Submit
-                            </Button>
+                    <div className='text-center' key={activity._id}>
 
-                            <Button
-                                type="submit"
-                                variant="contained"
-                                color="primary"
-                                className={classes.delete}
-                                onClick={props.handleDelete}
-                            >
-                                Delete
-                            </Button>
+                        <h3 className="font-weight-bold">{activity.name}</h3>
 
-                        </div>
-                    );
-                })}
+                        <p> {activity.description}</p>
+                        <Typography component="h5" variant="h6">
+                            Near By Restaurants
+                        </Typography>
+                        <FoodOptions
+                            activity={activity}
+                            setFoodVoted={setFoodVoted}
+                            foodVoted={foodVoted}
+                        />
+                        <hr />
+                        <Typography component="h5" variant="h6">
+                            Popular Movies on 'The Movie DB'
+                        </Typography>
+                        <MovieOptions
+                            activity={activity}
+                            setMovieVoted={setMovieVoted}
+                            movieVoted={movieVoted}
+                        />
+                        <Button
+                            type="submit"
+                            variant="contained"
+                            color="primary"
+                            className={classes.submit}
+                            onClick={handleSubmit}
+                        >
+                            Submit
+                        </Button>
+
+                    </div>
+
+                </div>
+
             </div>
 
-        </div>
-
-    ) : (
-        <h3>No Activities to display</h3>
+            <Link to="/savedactivities">← Back to Activities</Link>
+        </Container>
     );
-};
-// function refresh() {
-//     // make Ajax call here, inside the callback call:
-//     setTimeout(refresh, 5000);
-//     ActivityResult();
-// }
+}
 
-// // initial call, or just call refresh directly
-// setTimeout(refresh, 5000);
 
 export default ActivityResult;
